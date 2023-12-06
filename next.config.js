8abf87670b38cd7cc025b58a41769ff2ecdb510d
True-Fox <pes1202100682@pesu.pes.edu>
2023-12-06 14:32:56 +0530

    Update Config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-};
+  output: 'export',
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
